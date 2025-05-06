e601500cb1513d2bfd39f5564ae051f87ff6d4f6
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-06 02:05:19 -0400

    done with implementing navbar for the page

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+export default function Navbar(){
+
+
+
+    return <header className="navbar">
+        <nav>
+            <a href="#Home">Home</a>
+            <a href="#">Projects</a>
+            <a href="#">Contact Me</a>
+        </nav>
+    </header>
+}
