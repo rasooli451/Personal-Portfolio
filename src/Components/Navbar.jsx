3e1705e4993eb95860ad0597fd649a7c26fd323b
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-06 18:28:58 -0400

    edited navbar anchor tag to scroll to project section when clicked

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar(){
     return <header className="navbar">
         <nav>
             <a href="#Home">Home</a>
-            <a href="#">Projects</a>
+            <a href="#Projects">Projects</a>
             <a href="#">Contact Me</a>
         </nav>
     </header>
