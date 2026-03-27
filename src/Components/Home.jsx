126c142fd533758298a090b7ba43ef21e4851c48
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2026-03-26 22:16:06 -0400

    updated some of the texts from the homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,7 @@ import linkedin from "../assets/icons/linkedin-original.svg";
 import { useState, useEffect} from "react";
 
 export default function Home(){
-    const backgrounds = [{img : "books.jpg", description : "Sometimes in my free time, a book and a cup of coffee is enough to transfer me to another world where my imagination is unrestricted and anything is possible."}, {img : "Cinema.jpg", description : "As a dedicated cenephile, I can't appreciate cinema enough, I've watched most of the classics and have enjoyed modern masterpieces."}, {img : "games.jpg", description : "Part of the reason I am interested in game development is because I like playing games, I've been an active gamer since I was a child and still play sometimes."}, {img : "Ronaldo.jpg", description : "I love watching and playing Football, i've been obsessed with it ever since I started playing when I was little and still do sometimes."}];
+    const backgrounds = [{img : "books.jpg", description : "In my free time, I enjoy reading with a cup of coffee, it's a simple way for me to relax and get lost in a good story."}, {img : "Cinema.jpg", description : "I am a big movie buff and enjoy storytelling."}, {img : "games.jpg", description : "I've always enjoyed playing games, and that interest naturally led me to game development"}, {img : "Ronaldo.jpg", description : "I enjoy both watching and playing football, something I've been passionate about since I was young."}];
     const [currIndex, setIndex] = useState(0);
     let charIndex = 0;
     let typeSpeed = 100;
@@ -63,7 +63,7 @@ export default function Home(){
             <img src={profilepic} alt="Farhad Rasoli" className="profile"/>
             <div className="card">
                 <h2>About Me</h2>
-                <p>Hello there&#x1F44B;, welcome to my Portfolio website&#x1F60A;, I am <b>Farhad Rasoli</b>, a 2nd year Computer Science student at York University with a passion for web development and game development.</p>
+                <p>Hello there&#x1F44B;, welcome to my Portfolio website&#x1F60A;, I am <b>Farhad Rasoli</b>, a 4th year Computer Science student at York University with a passion for web development and game development.</p>
                 <div className="links">
                     <a href="https://github.com/rasooli451" target={"_blank"}><img src={github} alt="github" /></a>
                     <a href="https://www.linkedin.com/in/farhad-rasoli-7b7230294/" target={"_blank"}><img src={linkedin} alt="linkedin" /></a>
